027e95a5e6b7593c9f0b69c18b27e37c8786466c
Bbolive <rapatsar.bam@gmail.com>
2023-08-25 12:31:26 +0700

    code v.5 : pythonproject02

---
+++ test05.py
@@ -0,0 +1,11 @@
+# one by one
+# small to big
+print("Bangna" > "Bangbon")   #true
+print("a" > "A")              #True
+print("SAU" > "sau")          #False
+
+#logical Operator
+#and or not
+# ---------------------------------------------------->
+# * / % //, + - , < > , = == != <= >= , not , and , or
+print(7+3*2-5)
